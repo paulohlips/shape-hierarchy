cb5831a364ecd370ef5638dc855fdfc9241e7694
paulohlips <paulohfcmendes@gmail.com>
2023-12-16 16:32:37 -0300

    creates circle

---
+++ src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle extends  Square {
+    public Circle(String name, String color) {
+        super(name, color);
+    }
+
+    public double area(double radius) {
+        return  Math.PI * radius * radius;
+    }
+
+    public  double perimeter(double radius) {
+        return 2 * Math.PI * radius;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Square square = new Square("square", "red");
+        Circle circle = new Circle("circle", "blue");
 
         System.out.println(square.area(4));
         System.out.println(square.perimeter(10));
+
+        System.out.println(circle.area(1));
+        System.out.println(circle.perimeter(5));
 
     }
 }
